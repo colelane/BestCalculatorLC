082cc7703a43b8339702f44e3ccfbbed4fe0b530
Lane Coleman <lane.coleman@live.com>
2020-09-02 21:50:59 -0600

    Add project files.

---
+++ BestCalculatorLC.vb
@@ -0,0 +1,39 @@
+﻿Option Strict On
+Option Explicit On
+Option Compare Text
+Module BestCalculatorLC
+
+    Sub Main()
+        Dim str As String
+        Dim spl(2) As Object
+        Dim splitstr1 As Integer
+        Dim splitstr2 As String
+        Dim splitstr3 As Integer
+
+        Do
+            str = Console.ReadLine()
+
+            spl = Split(str, " ")
+
+
+            splitstr1 = CInt(spl(0))
+            splitstr2 = CStr(spl(1))
+            splitstr3 = CInt(spl(2))
+
+            If splitstr2 = "+" Then
+                Console.WriteLine(splitstr1 + splitstr3)
+            ElseIf splitstr2 = "-" Then
+                Console.WriteLine(splitstr1 - splitstr3)
+            ElseIf splitstr2 = "*" Then
+                Console.WriteLine(splitstr1 * splitstr3)
+            ElseIf splitstr2 = "/" Then
+                Console.WriteLine(splitstr1 / splitstr3)
+            End If
+
+            Console.ReadLine()
+            Console.Clear()
+        Loop
+
+    End Sub
+
+End Module
